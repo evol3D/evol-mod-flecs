314b586bbc06e16e3c6dfb32d4dab4ad206f01c2
Robear Selwans <robear.selwans@outlook.com>
2021-03-19 08:17:03 +0200

    Initial commit
    Signed-off-by: Robear Selwans <robear.selwans@outlook.com>

--- src/mod.c
+++ src/mod.c
@@ -7,7 +7,7 @@ EV_CONSTRUCTOR {
   printf("Loaded template module\n");
 }
 
-U32 update(F32 deltaTime)
+U32 _ev_world_update(F32 deltaTime)
 {
   EV_UNUSED_PARAM(deltaTime);
   return 0;
@@ -17,17 +17,7 @@ EV_DESTRUCTOR {
   printf("Unloaded template module\n");
 }
 
-EVMODAPI bool export_and(bool x, bool y)
-{
-  return x & y;
-}
-
-EVMODAPI bool export_or(bool x, bool y)
-{
-  return x | y;
-}
-
 EV_BINDINGS
 {
-  EV_NS_BIND_FN(Namespace1, update, update);
+  EV_NS_BIND_FN(World, update, _ev_world_update);
 }
